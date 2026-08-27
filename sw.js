f8b36ddf7608652cebbf3eef64cee4f40cce20d8
a0987183520 <a0987183520@gmail.com>
2026-08-27 15:04:57 +0800

    checkpoint: [v4.01] - 底端列修改為 App設計:陳新昱 並新增 我想建議 按鈕

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'xiaogu-pwa-cache-v4.00';
+const CACHE_NAME = 'xiaogu-pwa-cache-v4.01';
 
 const ASSETS_TO_CACHE = [
   'index.html',
   'app.html',
-  'css/style.css?v=4.00',
-  'js/app.js?v=4.00',
+  'css/style.css?v=4.01',
+  'js/app.js?v=4.01',
   'manifest.json',
   'z_img_app_192.png',
   'z_img_app_512.png',
